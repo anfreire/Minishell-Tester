369b27699ddc2168695ed515ceeed357fed920b7
André Freire Ferreira <100360644+anfreire@users.noreply.github.com>
2023-01-17 22:07:09 +0000

    Update test_minishell.sh

--- test_minishell.sh
+++ test_minishell.sh
@@ -6,6 +6,18 @@ write() {
     echo -n $message | awk '{for(i=1;i<=length;i++) {printf "%s",substr($0,i,1); fflush(); system("sleep 0.1")}}'
     echo ""
 }
+install_library(){
+	python3 -m venv Minienv	> /dev/null 2>&1
+	source Minienv/bin/activate	> /dev/null 2>&1
+    Minienv/bin/pip install --quiet subprocess > /dev/null 2>&1
+}
+
+display_message(){
+	clear	2< /dev/null
+	echo	2< /dev/null
+	echo	2< /dev/null
+	write "$(tput setaf 2)\033[1mSetting up a virtual environment and installing Python libraries ...\033[0m$(tput sgr0)"	2< /dev/null
+}
 clear	2< /dev/null
 echo	2< /dev/null
 write "$(tput setaf 2)\033[1m MINISHELL TESTER \033[0m$(tput sgr0)"	2< /dev/null
@@ -34,16 +46,4 @@ prompt_user() {
 
 prompt_user
 
-install_library(){
-	python3 -m venv Minienv	> /dev/null 2>&1
-	source Minienv/bin/activate	> /dev/null 2>&1
-    Minienv/bin/pip install --quiet subprocess > /dev/null 2>&1
-}
-
-display_message(){
-	clear	2< /dev/null
-	echo	2< /dev/null
-	echo	2< /dev/null
-	write "$(tput setaf 2)\033[1mSetting up a virtual environment and installing Python libraries ...\033[0m$(tput sgr0)"	2< /dev/null
-}
 
